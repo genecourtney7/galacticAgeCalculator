f1473c8c1e576b72a20201f39c3aead04a8c8a1f
gene courtney <genecourtney7@gmail.com>
2023-07-29 21:36:24 -0500

    Troubleshooting file issues. I had to redelete my .git file. ran first test, failed as expected. Added read me.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+
+
+window.addEventListener("load", function() {
+  console.log("loading and functioning")
+});
