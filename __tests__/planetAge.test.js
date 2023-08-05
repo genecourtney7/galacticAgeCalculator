f860da233dad835044f4b6fadc03e7c12a42ffb1
gene courtney <genecourtney7@gmail.com>
2023-08-04 21:12:25 -0500

    added two more tests, both run succesful

--- __tests__/planetAge.test.js
+++ __tests__/planetAge.test.js
@@ -5,8 +5,17 @@ describe('Planet', () => {
     const earthAge = 26;
 
     test('should correctly calculate age on Mercury', () => {
-        const expectedAge =earthAge / 0.241;
+        const expectedAge = earthAge / 0.241;
         expect(myPlanet.calculateAge(earthAge,"Mercury")).toBeCloseTo(expectedAge);
     });
     
+    test('should correctly calculate age on Venus', () => {
+        const expectedAge = earthAge / 0.615;
+        expect(myPlanet.calculateAge(earthAge,"Venus")).toBeCloseTo(expectedAge);
+    });
+
+    test('should correctly calculate age on Earth', () => {
+        expect(myPlanet.calculateAge(earthAge, "Earth")).toBe(earthAge);
+    });
+
 })
